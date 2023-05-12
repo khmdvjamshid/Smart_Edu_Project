bad43c5fa0575053144e67d6ebef38f231020bfb
Jamshid Khamidov <khmdvjamshid@gmail.com>
2023-05-12 20:45:20 +0300

    prepared for live

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-cghd$fd=x#(g12@+_m)(c#ps66j@69!t(g!bcw@zuv1@@gm$sy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ 
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -114,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # MEDIA FILES
